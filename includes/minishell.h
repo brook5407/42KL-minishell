908d669aeee6c696d2d6165bf502c8a62ec72df1
Ricky0625 <rickywong0625@gmail.com>
2022-12-02 13:06:32 +0800

    Added the libraries of the available functions

---
+++ includes/minishell.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 16:48:10 by wricky-t          #+#    #+#             */
+/*   Updated: 2022/12/01 16:56:23 by wricky-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* ====== LIBRARIES ====== */
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/resource.h>
+# include <sys/stat.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+# include <unistd.h>
+
+/* ====== TEXT STYLING ====== */
+
+/* ====== MACROS ====== */
+
+/* ====== ENUMS ====== */
+
+/* ====== STRUCTS ====== */
+
+/* ====== FUNCTION PROTOTYPES ====== */
+
+#endif
